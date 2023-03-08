388eba25e7bff23c156e4c2b736cc1cde82e92a6
deepan <deepanraj3112002@gmail.com>
2023-03-08 12:20:42 +0530

    Changes

--- backend/app.js
+++ backend/app.js
@@ -6,10 +6,20 @@ const bodyParser = require('body-parser');
 const dotenv =require('dotenv').config()
 
 // ===========================MIDDLEWARE============================
+const allowedOrigins = ['https://deep-full-auth.vercel.app'];
 
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
  
 app.use(express.json());
-app.use(cors()); 
+
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
