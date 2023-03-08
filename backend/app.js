bae4fb8a7e3be3e7316ceabdf97718808f076554
deepan <deepanraj3112002@gmail.com>
2023-03-08 12:26:25 +0530

    cors changes

--- backend/app.js
+++ backend/app.js
@@ -17,6 +17,14 @@ app.use(cors({
     }
   }
 }));
+
+//================ Enable CORS for all routes
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
  
 app.use(express.json());
 
