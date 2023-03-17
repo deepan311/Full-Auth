ce6fde8bd6968da183116938eb8e8283e4ed68f8
deepan <deepanraj3112002@gmail.com>
2023-03-17 08:07:25 +0530

    changes sim

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ app.use(cors({
 
 //================ Enable CORS for all routes
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', allowedOrigins);
   res.header('Access-Control-Allow-Credentials', true);
   next();
 });
